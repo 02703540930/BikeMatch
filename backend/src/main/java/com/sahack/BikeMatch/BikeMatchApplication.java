8a897dd285895ab1e630232eb71f1636dde1df6d
Sergio Alexandre Hack <sahack27@hotmail.com>
2021-09-07 20:18:43 -0300

    Projeto Criado

---
+++ backend/src/main/java/com/sahack/BikeMatch/BikeMatchApplication.java
@@ -0,0 +1,13 @@
+package com.sahack.BikeMatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikeMatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BikeMatchApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/sahack/BikeMatch/BikeMatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sahack.BikeMatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikeMatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
